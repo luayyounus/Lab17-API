ee621471f9796a4b920805a253d9b57d7d76d551
Luay <luayghazwan@yahoo.com>
2018-01-17 23:38:12 -0800

    Task Model created

---
+++ TaskManagerAPI/TaskManager/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{
+    public class Task
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
